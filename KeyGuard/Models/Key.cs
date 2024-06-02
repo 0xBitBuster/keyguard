3adfe93f7bad88c00669dbcd7e7439b7e4e11911
0xBitBuster <scharlt.coding@gmail.com>
2024-06-02 23:13:11 +0200

    Reduced keyguard file size

--- KeyGuard/Models/Key.cs
+++ KeyGuard/Models/Key.cs
@@ -12,42 +12,42 @@ namespace KeyGuard.Models
     [JsonObject(MemberSerialization.OptIn)]
     public partial class Key : ObservableValidator
     {
-        [JsonProperty]
+        [JsonProperty(PropertyName = "id")]
         [ObservableProperty]
         private string _id;
 
-        [JsonProperty]
+        [JsonProperty(PropertyName = "s")]
         [Required(ErrorMessage = "Service name is required.")]
         [MaxLength(100, ErrorMessage = "Service name must be 100 characters at most.")]
         [ObservableProperty]
         private string _service;
 
-        [JsonProperty]
+        [JsonProperty(PropertyName = "sl")]
         [MaxLength(400, ErrorMessage = "Link to service must be 400 characters at most.")]
         [ObservableProperty]
         private string _serviceLink;
 
-        [JsonProperty]
+        [JsonProperty(PropertyName = "u")]
         [MaxLength(100, ErrorMessage = "Username must be 100 characters at most.")]
         [ObservableProperty]
         private string _username;
 
-        [JsonProperty]
+        [JsonProperty(PropertyName = "e")]
         [EmailAddress(ErrorMessage = "Email address is invalid.")]
         [ObservableProperty]
         private string _email;
 
-        [JsonProperty]
+        [JsonProperty(PropertyName = "p")]
         [MaxLength(512, ErrorMessage = "Password must be 512 characters at most.")]
         [ObservableProperty]
         private string _password;
 
-        [JsonProperty]
+        [JsonProperty(PropertyName = "sq")]
         [MaxLength(400, ErrorMessage = "Security question must be 400 characters at most.")]
         [ObservableProperty]
         private string _securityQuestion;
 
-        [JsonProperty]
+        [JsonProperty(PropertyName = "o")]
         [MaxLength(2000, ErrorMessage = "Other information must be 2000 characters at most.")]
         [ObservableProperty]
         private string _other;
